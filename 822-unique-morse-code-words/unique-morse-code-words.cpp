f86e57333f74a91feae2fbdff8a2d3c2353dec87
Ritu Kumar <the.ritukumar@gmail.com>
2026-01-21 02:37:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (29.12%) - LeetSync

---
+++ 822-unique-morse-code-words/unique-morse-code-words.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+
+        vector<string> morse = {
+            ".-","-...","-.-.","-..",".","..-.","--.","....","..",
+            ".---","-.-",".-..","--","-.","---",".--.","--.-",".-.",
+            "...","-","..-","...-",".--","-..-","-.--","--.."
+        };
+
+        unordered_set<string> st;
+
+        for (string word : words) {
+
+            string code = "";
+
+            for (char c : word) {
+                code += morse[c - 'a'];
+            }
+
+            st.insert(code);
+        }
+
+        return st.size();
+    }
+};
